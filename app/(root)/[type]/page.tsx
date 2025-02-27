0a973a97de7c83b268bc1100c15272e5efa65e43
marymetilda <metildaphy@gmail.com>
2025-02-27 17:38:17 +0530

    feat: home screen

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,7 +1,7 @@
 import Card from "@/components/Card";
 import Sort from "@/components/Sort";
-import { getFiles } from "@/lib/actions/file.actions";
-import { getFileTypesParams } from "@/lib/utils";
+import { getFiles, getTotalSpaceUsed } from "@/lib/actions/file.actions";
+import { convertFileSize, getFileTypesParams } from "@/lib/utils";
 import { Models } from "node-appwrite";
 import React from "react";
 
@@ -9,10 +9,32 @@ const page = async ({ searchParams, params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
   const searchText = ((await searchParams)?.query as string) || "";
   const sort = ((await searchParams)?.sort as string) || "";
-
   const types = getFileTypesParams(type) as FileType[];
-
   const files = await getFiles({ types, searchText, sort });
+  const totalSpaceUsed = await getTotalSpaceUsed();
+  let size;
+
+  switch (type) {
+    case "documents":
+      size = totalSpaceUsed["document"]?.size;
+      break;
+
+    case "images":
+      size = totalSpaceUsed["image"]?.size;
+      break;
+
+    case "media":
+      size = totalSpaceUsed["audio"]?.size + totalSpaceUsed["video"].size;
+      break;
+
+    case "others":
+      size = totalSpaceUsed["other"]?.size;
+      break;
+
+    default:
+      size = 0;
+      break;
+  }
 
   return (
     <div className="page-container">
@@ -20,7 +42,7 @@ const page = async ({ searchParams, params }: SearchParamProps) => {
         <h1 className="h1 capitalize">{type}</h1>
         <div className="total-size-section">
           <p className="body-1">
-            Total: <span className="h5">0 MB</span>
+            Total: <span className="h5">{convertFileSize(size) || 0}</span>
           </p>
 
           <div className="sort-container">
